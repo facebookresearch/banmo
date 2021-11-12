595815485ead0d41f4e530f24343ae25a73214d6
Gengshan Yang <luanyingjian@gmail.com>
2021-11-12 13:17:42 -0800

    update nerfies baseline

---
+++ third_party/nerfies-0.1/run-swing.sh
@@ -0,0 +1,17 @@
+dev=0,1,2,3,4,5,6,7
+ngpu=8
+#dev=0
+#ngpu=1
+seqname=T_swing1
+logname=T_swing1
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.5
+export DATASET_PATH=dataset/$seqname
+export EXPERIMENT_PATH=logs/$logname
+#export NCCL_DEBUG=INFO 
+python train.py \
+    --data_dir $DATASET_PATH \
+    --exp_dir $EXPERIMENT_PATH \
+    --gin_configs configs/ablation.gin
+    #--gin_configs configs/test_vrig.gin
+    #--gin_configs configs/gpu_quarterhd.gin
+    #--gin_configs configs/gpu_quarterhd_4gpu.gin
